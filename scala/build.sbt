4d5ace34a45d445784b3e67d399da585c3fb9ff1
qilab- <qilab@gmx.com>
2016-02-28 19:10:20 +0900

    scalatest sorted

---
+++ scala/build.sbt
@@ -0,0 +1,14 @@
+name := "scala-sandbox"
+
+organization := "jp.qilab"
+
+version := "0.1.0-SNAPSHOT"
+
+scalaVersion := "2.11.7"
+
+libraryDependencies ++= Seq(
+  "org.scalatest" %% "scalatest" % "2.2.4" % "test"
+)
+
+scalacOptions in Compile ++= Seq("-unchecked", "-deprecation", "-feature", "-Xlint", "-Ywarn-dead-code", "-Ywarn-numeric-widen")
+
---
+++ scala/src/test/scala/jp/qilab/SortedInReverseOrderingSpec.scala
@@ -0,0 +1,22 @@
+package jp.qilab
+
+import org.scalatest.{FunSpec, Matchers}
+import org.scalatest.enablers.Sortable
+
+class SortedInReverseOrderingSpec extends FunSpec with Matchers {
+
+  describe("sortedInReverseOrder") {
+    it("should pass the test if the list is sorted in reverse order") {
+      val sortedInts = List(1, 4, 6, 7, 8, 12)
+      val reverseSortedInts = List(32, 21, 15, 3, 2, -1, -6)
+
+      val reverseOrdering = Ordering.by[Int, Int](_ * (-1))
+      val inReverseOrder: Sortable[List[Int]] = Sortable.sortableNatureOfSeq[Int, List](reverseOrdering)
+      // reverseSortedInts shouldBe (sorted) (inReverseOrder)
+      (reverseSortedInts shouldBe (sorted)) (inReverseOrder)
+      reverseSortedInts.shouldBe(sorted)(inReverseOrder)
+      reverseSortedInts.shouldBe(sorted)(Sortable.sortableNatureOfSeq[Int, List](implicitly[Ordering[Int]].reverse))
+    }
+  }
+
+}
